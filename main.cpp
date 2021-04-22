4fa3034a665ff8dbdd42cd39df2be348547eebc6
Adrian-Mok15 <adrian.mok15@myhunter.cuny.edu>
2021-04-21 22:28:27 -0400

    Added suggestions

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
 
 #include "funcs.h"
 
 int main()
 {
-  std::ifstream code;
-  code.open("input.cpp");
-  int tabs = 0;
-  std::string line;
-  while(getline(code, line)){
+    std::ifstream code;
+    code.open("input.cpp");
+    int tabs = 0;
+    std::string line;
+    std::vector<std::string> line_vector;//create a vector to store lines without tabs
+    while(getline(code, line)){
     line = removeLeadingSpaces(line);
+    line_vector.push_back(line);//save all the lines without tabs into the vector
     tabs = tabs - countChar(line, '}');
     for(int i = 0; i < tabs; i++){
       std::cout << "\t";
     }
     std::cout << line << '\n';
     tabs = tabs + countChar(line, '{');
+  }
+  for(int i = 0; i<line_vector.size(); ++i){//prints out the lines without the tabs
+      std::cout << line_vector[i] << "\n";
   }
   return 0;
 }
